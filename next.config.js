e0e1ee215a9d3f5ce7f2fd26ce8af70a410eaa1d
Luca Pattocchio <pattocchioluca@gmail.com>
2021-10-19 17:07:28 +0200

    fix(dependencies): add missing configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  //
   reactStrictMode: true,
-}
+
+  // webpack
+  webpack: (config, {webpack }) => {
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+};
