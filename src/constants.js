f7517044073778c79bf9d61dbef34c5d89571b2d
Luca Pattocchio <pattocchioluca@gmail.com>
2022-01-28 07:49:52 +0100

    chore: remove unused constants

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,3 @@
 export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const IS_TEST = process.env.NODE_ENV === 'test';
-
-/**********************
- * timestamp
- *********************/
-
-export const DATE = new Date();
