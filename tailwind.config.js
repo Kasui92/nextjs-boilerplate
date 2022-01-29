c8c4a6415e8b44172ad244c8c4db67ce92cebbcc
Luca Pattocchio <pattocchioluca@gmail.com>
2022-01-29 23:48:13 +0100

    fix: bad brackets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-      "./src/pages/**/*.[js,ts,jsx,tsx]",
-      "./src/client/**/*.[js,ts,jsx,tsx]",
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/client/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
