92ecc900e7151ecf7238707779095d3a0134e16c
Luca Pattocchio <pattocchioluca@gmail.com>
2022-01-30 17:35:18 +0100

    feat: implement next-seo pattern; update project reference (#5)

--- src/client/components/layouts/CommonLayout.tsx
+++ src/client/components/layouts/CommonLayout.tsx
@@ -1,8 +1,16 @@
+
+import {NextSeo} from "next-seo";
+
 import {ReactElement} from "react";
 import {WithChildren} from "../../lib/types";
 
 const CommonLayout = ({children}: WithChildren): JSX.Element =>
     <>
+
+        <NextSeo
+            titleTemplate={`%s | Next.js Boilerplate`}
+            description={`A basic structure to "play" with Next.js, powered by Typescript, styled with Tailwind Css and equipped with Docker`}
+        />
         {children}
     </>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
+import {NextSeo} from "next-seo";
+
 import {Page} from "../client/lib/types";
-import {getLayout} from "../client/components/layouts/CommonLayout";
 
 const IndexPage: Page = () => {
     return (
-        <div className='flex flex-col items-center justify-center w-full h-screen'>
-            <h1 className='text-3xl'>Hello World!</h1>
-        </div>
+        <>
+            <NextSeo
+                title={`Homepage`}
+            />
+            <div className='flex flex-col items-center justify-center w-full h-screen'>
+                <h1 className='text-3xl'>Next.js Boilerplate</h1>
+            </div>
+        </>
     )
 }
-
-IndexPage.Layout = getLayout
 
 export default IndexPage
