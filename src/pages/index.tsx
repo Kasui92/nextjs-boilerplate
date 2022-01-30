d1d0de1cc38d4cfbe762e4f3a12699d3857eacb9
Luca Pattocchio <pattocchioluca@gmail.com>
2022-01-30 15:13:10 +0100

    feat: implement next-seo pattern

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import {Page} from "../client/lib/types";
-import {getLayout} from "../client/components/layouts/CommonLayout";
+import {NextSeo} from "next-seo";
 
 const IndexPage: Page = () => {
     return (
-        <div className='flex flex-col items-center justify-center w-full h-screen'>
-            <h1 className='text-3xl'>Hello World!</h1>
-        </div>
+        <>
+            <NextSeo
+                title={`Homepage`}
+            />
+            <div className='flex flex-col items-center justify-center w-full h-screen'>
+                <h1 className='text-3xl'>Next.js Boilerplate</h1>
+            </div>
+        </>
     )
 }
-
-IndexPage.Layout = getLayout
 
 export default IndexPage
