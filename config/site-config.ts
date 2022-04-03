15934d7cd8ecfc2b080b9035ab948f417f69f470
Luca Pattocchio <pattocchioluca@gmail.com>
2022-04-03 21:39:18 +0200

    chore: add site-config

---
+++ config/site-config.ts
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+const siteConfig = {
+    meta: {
+        title: 'Next.js Boilerplate',
+        description: 'A basic structure to "play" with Next.js, powered by Typescript, styled with Tailwind Css and equipped with Docker',
+        favicon: '/favicon.ico',
+    },
+    author: {
+        name: 'Luca Pattocchio',
+        nickname: 'Kasui92',
+        url: 'https://lucapattocchio.dev',
+        email: 'luca.pattocchio@gmail.com'
+    }
+}
+
+export default siteConfig
